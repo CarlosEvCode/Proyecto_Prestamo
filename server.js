c48388ca865ce6cc68243a98067825317abcec3c
CarlosEvCode <programer.cm12@gmail.com>
2026-04-24 22:11:57 -0500

    feat: Agregar rutas y configuración del servidor

--- server.js
+++ server.js
@@ -11,3 +11,21 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routes
+app.use('/api/catalogos', require('./routes/catalogos'));
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/herramientas', require('./routes/herramientas'));
+app.use('/api/prestamos', require('./routes/prestamos'));
+app.use('/api/compras', require('./routes/compras'));
+app.use('/api/trabajadores', require('./routes/trabajadores'));
+
+app.get('/{*path}', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+//Inciar el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`);
+});
+
+module.exports = app;
